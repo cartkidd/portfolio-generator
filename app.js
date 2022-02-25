947c1feb9515864bcdc459dac075d6bac8ec66f0
Cartrell KIDD <cartrellk56@gmail.com>
2022-02-25 12:01:59 -0500

    for each learned

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
+// Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -11,6 +12,4 @@ const printProfileData = profileDataArr => {
   
     // Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
-
-  };
-  
+};
